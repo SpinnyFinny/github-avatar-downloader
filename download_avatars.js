3b49cdcf15ad039008005bf669dcc6fe643fac46
SpinnyFinny <finn.hiltner@hotmail.com>
2017-10-09 21:13:33 -0700

    request body, parsed

--- download_avatars.js
+++ download_avatars.js
@@ -4,11 +4,21 @@ var GITHUB_USER = "SpinnyFinny";
 var GITHUB_TOKEN = "a1f46648acf06c1c61d3085f8eacc24fbcaff27d";
 var repoOwner = 'tpope';
 var repoName = 'vim-commentary';
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+var requestURL = {
+  url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+  headers: {
+    "User-Agent": "SpinnyFinny"
+  }
+};
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  console.log(requestURL);
+  request(requestURL, function(err,response,body){
+
+      var info = JSON.parse(body)
+      console.log(info);
+
+    })
 }
 
 getRepoContributors('tpope', 'vim-commentary', function(err, result){
