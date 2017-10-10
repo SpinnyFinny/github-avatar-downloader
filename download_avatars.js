176a12c94615d97021da8d6db4dee6977ca2803c
SpinnyFinny <finn.hiltner@hotmail.com>
2017-10-10 08:37:41 -0700

    further linting

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,8 @@ var request = require('request');
 var fs = require('fs');
 var GITHUB_USER = "SpinnyFinny";
 var GITHUB_TOKEN = "a1f46648acf06c1c61d3085f8eacc24fbcaff27d";
-var repoOwner = process.argv[2]
-var repoName = process.argv[3]
+var repoOwner = process.argv[2];
+var repoName = process.argv[3];
 
 var requestURL = {
   url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
@@ -17,7 +17,7 @@ function getRepoContributors(owner, name, cb) {
     if (err) {
       cb(err);
     } else if (repoOwner === undefined || repoName === undefined){
-      cb('Error, please enter a user *and* repo')
+      cb('Error, please enter a user *and* repo');
       return;
     } else if (response.statusCode !== 200) {
       cb('Invalid user or repo');
@@ -35,21 +35,21 @@ function downloadImageByURL(url, filePath){
   request.get(url)
     .on('error', function(err){
       throw err;
-    })
+    });
     .pipe(fs.createWriteStream(filePath));
 }
 
-getRepoContributors(repoOwner, repoName, function(err, result){
+module.exports = getRepoContributors(repoOwner, repoName, function(err, result){
   if (err) {
     console.log('Error:', err);
     return;
   }
   userNamesAndAvatarURLs = [];
-  console.log('Downloading...')
+  console.log('Downloading...');
   result.forEach(function(user) {
     var filePath = `avatars/${user.login}.jpg`;
     var url = user.avatar_url;
-    downloadImageByURL(url, filePath)
+    downloadImageByURL(url, filePath);
   });
 });
 
