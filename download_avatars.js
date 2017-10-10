7cb285404d21ac833b9a76972ea4a4ab7038ccaa
SpinnyFinny <finn.hiltner@hotmail.com>
2017-10-09 20:46:12 -0700

    created requestURL variable

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,14 @@
 var request = require('request');
 
+var GITHUB_USER = "SpinnyFinny";
+var GITHUB_TOKEN = "a1f46648acf06c1c61d3085f8eacc24fbcaff27d";
+var repoOwner = 'tpope';
+var repoName = 'vim-commentary';
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
+  console.log(requestURL);
 }
 
 getRepoContributors('tpope', 'vim-commentary', function(err, result){
