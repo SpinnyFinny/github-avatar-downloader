c079f4ffe87da02f36cc1ef5c6fc86c0dc737d69
SpinnyFinny <finn.hiltner@hotmail.com>
2017-10-09 23:24:58 -0700

    added user input functionallity with process.argv

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,9 @@ var request = require('request');
 var fs = require('fs');
 var GITHUB_USER = "SpinnyFinny";
 var GITHUB_TOKEN = "a1f46648acf06c1c61d3085f8eacc24fbcaff27d";
-var repoOwner = 'tpope';
-var repoName = 'vim-commentary';
+var repoOwner = process.argv[2] //'tpope';
+var repoName = process.argv[3] //'vim-commentary';
+
 var requestURL = {
   url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
   headers: {
@@ -12,10 +13,13 @@ var requestURL = {
 };
 
 
-function getRepoContributors(repoOwner, repoName, cb) {
+function getRepoContributors(owner, name, cb) {
   request(requestURL, function(err, response, body){
+
     if (err) {
       cb(err);
+    } else if (repoOwner === undefined || repoName === undefined){
+      console.log('Error, please entry a user and repo')
     } else {
       var userInfo = JSON.parse(body);    // note that if we weren't busy, we'd check to see if `body` is even parseable, ha ha ha, be srs
       cb(null, userInfo);
@@ -42,7 +46,7 @@ function downloadImageByURL(url, filePath){
 
 }
 
-getRepoContributors("fuck", "your mom", function(err, result){
+getRepoContributors(repoOwner, repoName, function(err, result){
   if (err) {
     console.log('Errors:', err);
   } else {
