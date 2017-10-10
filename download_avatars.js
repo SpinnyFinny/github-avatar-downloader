c12a536f55327fa67bf95a122efac74e9f832914
SpinnyFinny <finn.hiltner@hotmail.com>
2017-10-09 20:37:08 -0700

    added getRepoContributers function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,12 @@
 var request = require('request');
 
-console.log("Welcome to the GitHub Avatar Downloader");
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+}
+
+getRepoContributors('tpope', 'vim-commentary', function(err, result){
+  console.log('Errors:', err);
+  console.log('Results:', result);
+
+});
